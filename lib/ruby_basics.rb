2794cbd355975bb0f2e6f122449dc8dc75572d19
Christopher Weber <chrisrrweber@gmail.com>
2018-08-15 03:27:14 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,17 +1,17 @@
 def division(num1, num2)
- 
+  return num1 / num2
 end
 
 def assign_variable(value)
-  
+  return value
 end
 
-def argue
-  
+def argue(argument)
+  return argument
 end
 
-def greeting
-
+def greeting(greeting, name)
+  return 
 end
 
 def return_a_value
